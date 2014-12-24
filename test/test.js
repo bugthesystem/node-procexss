90fca72b692887eda140f4264c2b8a3fbe80e4dc
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-12-24 09:10:52 +0200

    init

---
+++ test/test.js
@@ -0,0 +1,17 @@
+
+process.env.NODE_ENV = 'test';
+
+var assert = require('assert');
+var connect = require('connect');
+var http = require('http')
+
+var request = require('supertest');
+var url = require('url')
+
+var procexss = require('..')
+
+describe('procexss', function () {
+  it('should work in req.body', function(done) {
+   
+  });
+});
